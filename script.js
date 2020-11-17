77dc78ca40f66b28ee54e0ac8dc3c6767adbda19
daniela <danielareijs@gmail.com>
2020-11-17 22:31:10 +0100

    -

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 
+
 var currentEquation;
 var currentAnswer;
 var timeLeft;
 var timer;
+var score = 0;
 
 var getRandomEquation = function(){
   var num1 = Math.floor(Math.random() * 10) + 1;
@@ -19,9 +21,11 @@ var checkAnswer = function(){
     if(answerValue == currentAnswer){
     $('#answer').val('');
     getRandomEquation()
+    timeLeft ++;
+    score ++;
+    $('#score').text(score);
  }
 };
-
 
 var countDown = function(){
   $('#secondsLeft').text(timeLeft);
@@ -29,22 +33,24 @@ var countDown = function(){
       timeLeft -= 1;
     } else {
       clearInterval(timer);
+      $('#playBtn').text('Play Again').removeClass('hide');
+      score = 0;
     }
   }
 
 
-
-$('#playBtn').on('click', function(){
-    timeLeft = 10;
-    timer = setInterval(countDown, 1000)
-    currentEquation = getRandomEquation();
-})
-
-$('#answer').on('keyup', function(){
-    checkAnswer();
+  $('#playBtn').on('click', function(){
+      timeLeft = 10;
+      $('#score').text(score);
+      timer = setInterval(countDown, 1000);
+      currentEquation = getRandomEquation();
+      $('#playBtn').addClass('hide');
   })
 
 
+  $('#answer').on('keyup', function(){
+    checkAnswer();
+  })
 
 
 });
