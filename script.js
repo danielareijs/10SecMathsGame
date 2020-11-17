ff45048a2f92403c466cf5a3eff143e3ebd4904a
daniela <danielareijs@gmail.com>
2020-11-17 21:36:03 +0100

    Start button and get first equation

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
 
 
+var currentEquation;
+var currentAnswer;
 
+var getRandomEquation = function(){
+  var num1 = Math.floor(Math.random() * 10) + 1;
+  var num2 = Math.floor(Math.random() * 10) + 1;
+  var equation = num1 + ' + ' + num2;
+  currentAnswer = num1+num2;
+  currentEquation = equation;
+  $('#mathEquation').text(equation);
+}
+
+
+$('#playBtn').on('click', function(){
+    currentEquation = getRandomEquation();
+})
 
 
 });
